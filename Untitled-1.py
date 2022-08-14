cce499cf578bc7fa1bd0f22381b34e84b4ddac61
MartinOviedo13 <104042360+MartinOviedo13@users.noreply.github.com>
2022-08-14 19:46:26 -0300

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,9 @@
+
+dueno= [28957346, "Juan", "Perez", 4789689,"Belgrano 101"]
+
+for i in dueno:
+    dueno[0]>26000000
+    print (dueno[3])
+    break
+
+dueno2=[23546987,"Maria", "Perez", 4789689, "Pueyrredon 811"]
---
+++ Untitled-2.py
@@ -0,0 +1,11 @@
+from ast import Delete
+import numbers
+import string
+
+
+dueno2=[23546987,"Maria", "Perez", 4789689, "Pueyrredon 811"]
+
+for x in dueno2:
+    print(x)
+    if x == dueno2[0]:
+        continue
